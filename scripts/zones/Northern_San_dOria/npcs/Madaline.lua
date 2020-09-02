4c7658859405274f65bf0b214d3b4c7aabaed731
Era-Lusiphur <sf.chakan@gmail.com>
2020-09-02 09:43:19 -0400

    Crash fix for Madaline and Telmoda
    Resolves this issue https://github.com/project-topaz/topaz/issues/1055

--- scripts/zones/Northern_San_dOria/npcs/Madaline.lua
+++ scripts/zones/Northern_San_dOria/npcs/Madaline.lua
@@ -10,11 +10,7 @@ end
 function onTrigger(player, npc)
     local Telmoda_Madaline = player:getCharVar("Telmoda_Madaline_Event")
     if (Telmoda_Madaline ~= 1) then
-<<<<<<< Updated upstream
-        player:setCharVar(player, "Telmoda_Madaline_Event", 1)
-=======
         player:setCharVar("Telmoda_Madaline_Event", 1)
->>>>>>> Stashed changes
         player:startEvent(531)
     else
         player:startEvent(617)
@@ -25,8 +21,4 @@ function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
-<<<<<<< Updated upstream
 end
-=======
-end
->>>>>>> Stashed changes
--- scripts/zones/Northern_San_dOria/npcs/Telmoda.lua
+++ scripts/zones/Northern_San_dOria/npcs/Telmoda.lua
@@ -11,11 +11,7 @@ function onTrigger(player, npc)
     local Telmoda_Madaline = player:getCharVar("Telmoda_Madaline_Event")
 
     if (Telmoda_Madaline ~= 1) then
-<<<<<<< Updated upstream
-        player:setCharVar(player, "Telmoda_Madaline_Event", 1)
-=======
         player:setCharVar("Telmoda_Madaline_Event", 1)
->>>>>>> Stashed changes
         player:startEvent(531)
     else
         player:startEvent(616)
@@ -26,8 +22,4 @@ function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
-<<<<<<< Updated upstream
 end
-=======
-end
->>>>>>> Stashed changes
